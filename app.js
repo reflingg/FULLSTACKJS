16cc49698b4eb2f4f2b39eec0f7b6978eafea534
Refling <88033767+reflingg@users.noreply.github.com>
2024-10-18 22:16:36 +0000

    update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Exercise 1
 let firstFavNum = 7;
 let secondFavNum = 4;
 console.log(firstFavNum + secondFavNum);
@@ -6,3 +7,13 @@ console.log(firstFavNum * secondFavNum);
 console.log(firstFavNum / secondFavNum);
 console.log(firstFavNum % secondFavNum);
 console.log(firstFavNum ** secondFavNum);
+
+
+//// Boolean True or False 
+// we have defined and Undefined
+
+// 2.
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let favNumb = 7;
+console.log(favNumb + undefined)
