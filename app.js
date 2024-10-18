de238a367c249eb846ed74c1d18f968ec7d8fa85
Refling <88033767+reflingg@users.noreply.github.com>
2024-10-18 21:52:53 +0000

    update

---
+++ app.js
@@ -0,0 +1,8 @@
+let firstFavNum = 7;
+let secondFavNum = 4;
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** secondFavNum);
