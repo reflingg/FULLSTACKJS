e18a8a4f5ab58dd267cc2aa5a15a30cda5a7289a
Refling <88033767+reflingg@users.noreply.github.com>
2024-10-19 10:04:44 +0000

    updates

--- app.js
+++ app.js
@@ -18,15 +18,24 @@ let isJsHard = false;
 let favNumb = 7;
 console.log(favNumb + undefined)
 
-// OPERATORS
+//Comparision  OPERATORS
+//--------------------------------------
 // RELATIONAL | EQUALITY
 
 // Relational Operators
-console.log(10 > 10); // false
-console.log(10 < 10); // false
-console.log(10 >= 10); // true
-console.log(10 <= 10); // true
+// console.log(10 > 10); // false
+// console.log(10 < 10); // false
+// console.log(10 >= 10); // true
+// console.log(10 <= 10); // true
 
 // Equality     Operators
 // === strict equality (type + value)
 // !== strict non-equality operator (type + value)
+// == lose equality operator (value)
+// != lose not equality operator (value)
+
+// console.log(10 === 10);
+// console.log(10 !== 10);
+// console.log(10 == "10");
+// console.log(10 != 10);
+
