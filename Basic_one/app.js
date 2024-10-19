bea5085eb2ba7004a5c2dc6d9fc17f669726dd34
reflingg <mosesolayinkaa@gmail.com>
2024-10-19 23:57:21 +0100

    updates

---
+++ Basic_one/app.js
@@ -0,0 +1,41 @@
+// Variables
+// Exercise
+// let names = "Moses Olayinka";
+// let whatDoYouWannaBecomeInLife = "Programmer";
+// let gender = "Male";
+// let twitterHandle = "https://x.com/reflingg";
+// console.log(names);
+// console.log(whatDoYouWannaBecomeInLife);
+// console.log(twitterHandle);
+// console.log(gender);
+
+// Primitive Types
+// let num = 10;
+// console.log(typeof num);
+
+//EXERCISE
+// let firstFavNum = 3;
+// let secondFavNum = 2;
+// console.log(firstFavNum + secondFavNum);
+// console.log(firstFavNum - secondFavNum);
+// console.log(firstFavNum * secondFavNum);
+// console.log(firstFavNum / secondFavNum);
+// console.log(firstFavNum % secondFavNum);
+// console.log(firstFavNum ** secondFavNum);
+
+////  Boolean True or False  /////////////.
+// we have defined and Undefined
+
+// Exercise 2.
+// let isJsProgrammingLanguage = true;
+// let isJsHard = false;
+// let favNumb = 7;
+// console.log(favNumb + undefined)
+
+// console.log(firstName.toLowerCases())
+// console.log(firstName.toLUpperCases())
+
+// 5. Slice
+// console.log(firstName.slice(0, 1));
+
+// 6. split & Join
