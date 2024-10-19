2c95ed6086462f3c5b3d5657bdbc5b203074880e
Refling <88033767+reflingg@users.noreply.github.com>
2024-10-19 08:15:49 +0000

    updates

--- app.js
+++ app.js
@@ -26,3 +26,7 @@ console.log(10 > 10); // false
 console.log(10 < 10); // false
 console.log(10 >= 10); // true
 console.log(10 <= 10); // true
+
+// Equality     Operators
+// === strict equality (type + value)
+// !== strict non-equality operator (type + value)
