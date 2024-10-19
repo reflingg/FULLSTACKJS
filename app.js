6c8b0a5e4731719318d9343ca18615bd6d387523
Refling <88033767+reflingg@users.noreply.github.com>
2024-10-19 10:14:36 +0000

    updates

--- app.js
+++ app.js
@@ -1,41 +1,51 @@
 // Exercise 1
-let firstFavNum = 7;
-let secondFavNum = 4;
-console.log(firstFavNum + secondFavNum);
-console.log(firstFavNum - secondFavNum);
-console.log(firstFavNum * secondFavNum);
-console.log(firstFavNum / secondFavNum);
-console.log(firstFavNum % secondFavNum);
-console.log(firstFavNum ** secondFavNum);
+// let firstFavNum = 7;
+// let secondFavNum = 4;
+// console.log(firstFavNum + secondFavNum);
+// console.log(firstFavNum - secondFavNum);
+// console.log(firstFavNum * secondFavNum);
+// console.log(firstFavNum / secondFavNum);
+// console.log(firstFavNum % secondFavNum);
+// console.log(firstFavNum ** secondFavNum);
 
 
 //// Boolean True or False 
 // we have defined and Undefined
 
 // Exercise 2.
-let isJsProgrammingLanguage = true;
-let isJsHard = false;
-let favNumb = 7;
-console.log(favNumb + undefined)
+// let isJsProgrammingLanguage = true;
+// let isJsHard = false;
+// let favNumb = 7;
+// console.log(favNumb + undefined)
 
 //Comparision  OPERATORS
 //--------------------------------------
 // RELATIONAL | EQUALITY
 
-// Relational Operators
+// Relational Operators(>, <, >=, <=)
 // console.log(10 > 10); // false
 // console.log(10 < 10); // false
 // console.log(10 >= 10); // true
 // console.log(10 <= 10); // true
 
-// Equality     Operators
+// Equality Operators
 // === strict equality (type + value)
 // !== strict non-equality operator (type + value)
 // == lose equality operator (value)
 // != lose not equality operator (value)
-
 // console.log(10 === 10);
 // console.log(10 !== 10);
 // console.log(10 == "10");
 // console.log(10 != 10);
 
+// Exercise 3.
+let firstFavNum = 7;
+let secondFavNum = 4;
+// console.log(firstFavNum > secondFavNum); // greater than
+// console.log(firstFavNum < secondFavNum); // lesser than
+// console.log(firstFavNum >= secondFavNum); // greater than| equal
+// console.log(firstFavNum <= secondFavNum); // lesser than| equal
+// console.log(firstFavNum === secondFavNum); // strict equal
+// console.log(firstFavNum == secondFavNum); // loose equal
+// console.log(firstFavNum !== secondFavNum); // strict not equal
+// console.log(firstFavNum != secondFavNum); // loose nt equals
