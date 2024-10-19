a26598d469726fff8477c58ed355343826305117
reflingg <mosesolayinkaa@gmail.com>
2024-10-20 00:59:52 +0100

    updating exercise 4

--- Basic_one/app.js
+++ Basic_one/app.js
@@ -72,4 +72,29 @@
 // console.log(firstName.slice(0, 1));
 
 // 6. split & Join
-// console.log(firstName.split());
+// console.log(firstName.split("").join(""));
+
+// 7. Includes
+// console.log(firstName.includes("o"));
+
+// 8. Trim
+//console.log(firstName.trim());
+
+/// Back Ticks //// (``)
+
+// let firstName = "Moses";
+// let lastName = "Olayinka";
+
+// let fullName = `${firstName} ${lastName}`;
+// console.log(fullName);
+
+/// Exercise 4
+
+// let favActorFirstName = "Diesel";
+// let favActorLastName = "Washington";
+// let favActorFullName = favActorFirstName + " " + favActorLastName;
+// console.log(favActorFullName);
+// let uppercase = favActorFullName.toUpperCase();
+// let message = `My favorite Actor is ${uppercase} He has a great personality`;
+// message += "his best show is Silicon Valley";
+// console.log(message);
